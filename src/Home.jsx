a74601a0ee1cddf100b5daf4e4e479d767c5df44
Mark <mwhancock2@gmail.com>
2023-07-27 14:19:01 -0230

    create home component

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+export default function Home() {
+    return (
+        <>
+            <div className="page-content">
+                <section id="home" className="page">
+                    <section id="result_content">
+                        <div className="search-lable">
+                            <div>
+                                <h2>Search Results</h2>
+                            </div>
+                            <hr />
+                            <button id="close-search" className="close-btn">X</button>
+                        </div>
+                        <section id="search_results" className="grid-box games"></section>
+                    </section>
+                </section>
+            </div>
+        </>
+    )
+}
