308ffbac0fb9444074f57414d700cf95fc932692
Mark <mwhancock2@gmail.com>
2023-08-30 23:13:57 -0230

    Implement routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './App.css';
 import { Home } from './pages';
-import { Dialog } from './components/home';
+import { Dialog, Footer, Nav } from './components/home';
 import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <>
-      <main className="grid-box main-grid">
+     <Nav />
+      <main className="container">
         <Dialog />
         <Routes>
             <Route path="/" element={< Home />} />
         </Routes>
       </main>
+      <Footer/>
     </>
   );
 }
