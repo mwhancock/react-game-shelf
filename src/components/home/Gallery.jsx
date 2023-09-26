bffb08f03e1a027c91af015476126c86a0444749
Mark <mwhancock2@gmail.com>
2023-09-25 21:32:44 -0230

    begin styling feature cards

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -1,72 +1,64 @@
 import { useEffect, useState } from "react";
 
-const GameCard = ({ game, isActive }) => {
-  return (
-    <div className="card mb-3" style={{maxWidth: 540}}>
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img src={game.img} class="img-fluid rounded-start" alt="..." />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p className="card-text">
-              <small class="text-body-secondary">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const GameCard = ({ game, isActive }) => {
+//   return (
+//     <div className="card mb-3" style={{maxWidth: 540}}>
+//       <div className="row g-0">
+//         <div className="col-md-4">
+//           <img src={game.img} class="img-fluid rounded-start" alt="..." />
+//         </div>
+//         <div className="col-md-8">
+//           <div className="card-body">
+//             <h5 className="card-title">Card title</h5>
+//             <p className="card-text">
+//               This is a wider card with supporting text below as a natural
+//               lead-in to additional content. This content is a little bit
+//               longer.
+//             </p>
+//             <p className="card-text">
+//               <small class="text-body-secondary">Last updated 3 mins ago</small>
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-const getAtlasData = (params = "order_by=rank&limit=100") => {
-  const clientID = "9RQI1WBCZA";
 
-  return fetch(
-    `https://api.boardgameatlas.com/api/search?${params}&client_id=${clientID}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data.games;
-    })
-    .catch((err) => console.log("ERROR: ", err));
-};
+
+
+
 
 export default function Gallery() {
-  const [games, setGames] = useState([]);
-  const [index, setIndex] = useState(0);
+  // const [games, setGames] = useState([]);
+  // const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    getAtlasData("order_by=trending&limit=15")
-      .then((gameList) => {
-        const recGames = gameList.map((game) => ({
-          id: game.id,
-          name: game.name,
-          img: game?.images?.large,
-          description: game.description_preview,
-        }));
-        setGames(recGames);
-      })
-      .catch((err) => console.log("ERROR: ", err));
-  }, []);
+  // useEffect(() => {
+  //   getAtlasData("order_by=trending&limit=15")
+  //     .then((gameList) => {
+  //       const recGames = gameList.map((game) => ({
+  //         id: game.id,
+  //         name: game.name,
+  //         img: game?.images?.large,
+  //         description: game.description_preview,
+  //       }));
+  //       setGames(recGames);
+  //     })
+  //     .catch((err) => console.log("ERROR: ", err));
+  // }, []);
 
-  const fwdSlide = () => {
-    setIndex((prevIndex) => (prevIndex < games.length - 1 ? prevIndex + 1 : 0));
-  };
+  // const fwdSlide = () => {
+  //   setIndex((prevIndex) => (prevIndex < games.length - 1 ? prevIndex + 1 : 0));
+  // };
 
-  const bwdSlide = () => {
-    setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : games.length - 1));
-  };
+  // const bwdSlide = () => {
+  //   setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : games.length - 1));
+  // };
 
   return (
     <>
-      <div className="label featured">
+      {/* <div className="label featured">
         <h2>Feaured</h2>
       </div>
       <hr />
@@ -87,7 +79,23 @@ export default function Gallery() {
             );
           })}
         </div>
-      </div>
+      </div> */}
+
+
+
+
+
+
+<div className="card lg:card-side bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div>
     </>
   );
 }
