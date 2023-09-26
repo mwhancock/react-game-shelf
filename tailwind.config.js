0a51e845c59bc59fec9ccbd7443ea27b4c4dd04d
Mark <mwhancock2@gmail.com>
2023-09-25 21:30:33 -0230

    add custom styles

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    './src/**/*.{html,ts,css,js,jsx,tsx}',
+    
+  ],
+  theme: {
+    colors: {
+      'accent': '#825574',
+      'alt-accent': '#cfb0c5',
+      'bg-color': '#fff4e0',
+      'dark-green': '#8dac87',
+      'light-green': '#b4cfb0c4',
+      'logo-color': '#263330',
+      'text-color': '#384541',
+    },
+    extend: {
+      borderRadius: {
+        'br-main': '20px',
+    },
+      boxShadow: {
+        'box-shadow': '0 0 20px #384541',
+      }
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [],
+}
+}
+
