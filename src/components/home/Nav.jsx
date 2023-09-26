81ab37266cc48ab0f3eb163d4a183220281aae15
Mark <mwhancock2@gmail.com>
2023-09-25 21:32:18 -0230

    begin styling navbar

--- src/components/home/Nav.jsx
+++ src/components/home/Nav.jsx
@@ -1,46 +1,20 @@
 export default function Nav() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="">
-                Home
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="">
-                Features
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="">
-                Pricing
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
-            </li>
-          </ul>
+  <>
+    <nav className="navbar navbar-expand-lg tw-bg-dark-green tw-h-20 tw-shadow-box-shadow tw-sticky tw-top-0 tw-z-50">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="../../App.js"><img src="../../img/logo.png" alt="" />Logo</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <form class="d-flex tw-w-[50%] tw-ml-[27rem] tw-drop-shadow-md" role="search">
+                    <input className="form-control me-2 tw-rounded-[20px]" type="search" placeholder="Search..." aria-label="Search" />
+                    <button className="btn btn-outline-success tw-bg-alt-accent tw-text-text-color  hover:tw-bg-accent tw-border-none" type="submit">Search</button>
+                </form>
+            </div>
         </div>
-      </div>
     </nav>
-  );
+</>
+  )
 }
