433d9a69c330b479be3ca2ec99a677acd075ab77
Mark <mwhancock2@gmail.com>
2023-09-25 22:55:30 -0230

    add api call to get game info for feature cards

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -1,29 +1,25 @@
 import { useEffect, useState } from "react";
+import { getHotIds, getGameDetails } from "../BGGApi";
 
-// const GameCard = ({ game, isActive }) => {
-//   return (
-//     <div className="card mb-3" style={{maxWidth: 540}}>
-//       <div className="row g-0">
-//         <div className="col-md-4">
-//           <img src={game.img} class="img-fluid rounded-start" alt="..." />
-//         </div>
-//         <div className="col-md-8">
-//           <div className="card-body">
-//             <h5 className="card-title">Card title</h5>
-//             <p className="card-text">
-//               This is a wider card with supporting text below as a natural
-//               lead-in to additional content. This content is a little bit
-//               longer.
-//             </p>
-//             <p className="card-text">
-//               <small class="text-body-secondary">Last updated 3 mins ago</small>
-//             </p>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const GameCard = ({ game, isActive }) => {
+  return (
+    <div className="card mb-3" style={{maxWidth: 540}}>
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={game[0].image} className="img-fluid rounded-start" alt="..." />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{game[0].name}</h5>
+            <p className="card-text">
+              {game[0].description}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 
 
@@ -31,34 +27,30 @@ import { useEffect, useState } from "react";
 
 
 export default function Gallery() {
-  // const [games, setGames] = useState([]);
-  // const [index, setIndex] = useState(0);
+  const [games, setGames] = useState([]);
+  const [index, setIndex] = useState(0);
 
-  // useEffect(() => {
-  //   getAtlasData("order_by=trending&limit=15")
-  //     .then((gameList) => {
-  //       const recGames = gameList.map((game) => ({
-  //         id: game.id,
-  //         name: game.name,
-  //         img: game?.images?.large,
-  //         description: game.description_preview,
-  //       }));
-  //       setGames(recGames);
-  //     })
-  //     .catch((err) => console.log("ERROR: ", err));
-  // }, []);
+  useEffect(() => {
+    getHotIds().then((hotlist) => {
+    const promises = hotlist.map((id) => getGameDetails(id));
+    Promise.all(promises).then((game) => {
+      setGames(game);
+    });
+  });
 
-  // const fwdSlide = () => {
-  //   setIndex((prevIndex) => (prevIndex < games.length - 1 ? prevIndex + 1 : 0));
-  // };
+  }, []);
 
-  // const bwdSlide = () => {
-  //   setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : games.length - 1));
-  // };
+  const fwdSlide = () => {
+    setIndex((prevIndex) => (prevIndex < games.length - 1 ? prevIndex + 1 : 0));
+  };
+
+  const bwdSlide = () => {
+    setIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : games.length - 1));
+  };
 
   return (
     <>
-      {/* <div className="label featured">
+      <div className="label featured">
         <h2>Feaured</h2>
       </div>
       <hr />
@@ -71,21 +63,17 @@ export default function Gallery() {
           <span className="material-symbols-outlined">arrow_forward_ios</span>
         </button>
         <div>
-          {games.map((game, newIndex) => {
-            return (
-              <div key={game.id}>
-                <GameCard game={game} isActive={newIndex === index} />
-              </div>
-            );
-          })}
+          {games.map((game) => (
+            <GameCard key={game.id} game={game} isActive={index} />
+          ))}
         </div>
-      </div> */}
+      </div>
 
 
 
 
 
-
+{/* 
 <div className="card lg:card-side bg-base-100 shadow-xl">
   <figure><img src="/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
   <div className="card-body">
@@ -95,7 +83,7 @@ export default function Gallery() {
       <button className="btn btn-primary">Listen</button>
     </div>
   </div>
-</div>
+</div> */}
     </>
   );
 }
