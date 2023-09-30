933c5fccff2727e3770ed76e9a4d23a8b062f70a
Mark <mwhancock2@gmail.com>
2023-09-30 15:27:57 -0230

    adjust button style and placement

--- src/components/home/Gallery.jsx
+++ src/components/home/Gallery.jsx
@@ -5,13 +5,13 @@ const GameCard = ({ game,isActive }) => {
    
   return (
     <>
-      <div data-theme="light"  id={game[0].id} className={`carousel-item card ml-96 relative lg:card-side rounded-2xl bg-light-green shadow-xl mt-10 w-[72rem] h-[28rem] hidden`}>
-        <figure><img className="w-[30rem] h-96 rounded-xl shadow-lg ml-10" src={game[0].image}  alt="box cover"/></figure>
+      <div data-theme="light"  id={game[0].id} className={`carousel-item card ml-96 relative lg:card-side rounded-2xl bg-light-green shadow-xl mt-10 w-[73rem] h-[28rem] hidden`}>
+        <figure><img className="w-[30rem] h-96 rounded-xl shadow-lg ml-16" src={game[0].image}  alt="box cover"/></figure>
         <div className="card-body">
           <h2 className="card-title text-text-color">{game[0].name}</h2>
           <p className="line-clamp-[7] w-[33rem] leading-6 text-text-color" dangerouslySetInnerHTML={{ __html: game[0].description }}></p>
           <div className="card-actions justify-end">
-            <button className="absolute top-4 left-[41.8%] py-1.5 px-3.5 ml-2 rounded-md font-black text-3xl bg-accent text-btn-text hover:text-text-color  hover:bg-alt-accent border-none eas-in duration-150">+</button>
+            <button className="absolute top-4 left-[43.2%] py-1.5 px-3.5 ml-2 rounded-md font-black text-3xl bg-accent text-btn-text hover:text-text-color  hover:bg-alt-accent border-none eas-in duration-150">+</button>
           </div>
         </div>
       </div>
@@ -65,12 +65,11 @@ export default function Gallery() {
             <GameCard key={game.id} game={game} isActive={cardIndex}/>
             ))}
             <div className="absolute flex justify-between transform -translate-y-1/2 left-96 right-96 top-[43%]">
-              <button className="" type="button" data-bs-target="#gallery" data-bs-slide="prev" onClick={decrementCardIndex}>
-                <span className="text-3xl ml-3" aria-hidden="true">❮</span>
+              <button className="hover:bg-dark-green ease-in duration-200 h-36 hover:rounded-2xl w-10" type="button" data-bs-target="#gallery" data-bs-slide="prev" onClick={decrementCardIndex}>
+                <span className="text-3xl hover:text-black">❮</span>
               </button>
-              <button className="carousel-control-next" type="button" data-bs-target="#gallery" data-bs-slide="next" onClick={incrementCardIndex}>
-                  <span className="" aria-hidden="true"></span>
-                  <span className="text-3xl">❯</span>
+              <button className="hover:bg-dark-green ease-in duration-200 h-36 hover:rounded-2xl w-10" type="button" data-bs-target="#gallery" data-bs-slide="next" onClick={incrementCardIndex}>
+                  <span className="text-3xl hover:text-black">❯</span>
               </button> 
             </div>
         </div>
