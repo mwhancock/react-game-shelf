4cfc987ed9ec689041244c0f3af90e574e9e22a6
Mark <mwhancock2@gmail.com>
2023-10-11 23:51:26 -0230

    move library template to own component

--- src/components/home/Library.jsx
+++ src/components/home/Library.jsx
@@ -2,13 +2,6 @@ export default function Library() {
     return (
         <>
             <section id="library" className="page">
-                <section id="result_content">
-                    <div className="label">
-                        <h2>Search Results</h2>
-                    </div>
-                    <hr />
-                    <section id="search_results" className="grid-box games"></section>
-                </section>
                 <div className="label">
                     <h2>Library</h2>
                 </div>
