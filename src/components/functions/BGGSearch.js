aca869f06e98556df521bdf85696fd49972152f7
Mark <mwhancock2@gmail.com>
2023-10-11 23:53:45 -0230

    begin work on displaying search results

--- src/components/functions/BGGSearch.js
+++ src/components/functions/BGGSearch.js
@@ -1,3 +1,5 @@
+import { SearchResultCard } from "../home/GameCards";
+
 const searchGameByName = (name) => {
     const searchResults = fetch(`https://boardgamegeek.com/xmlapi2/search?query=${name}&type=boardgame`)
         .then((res) => res.text())
@@ -5,8 +7,15 @@ const searchGameByName = (name) => {
         const parser = new DOMParser();
         const xmlData = parser.parseFromString(data, "text/xml");
         const gameList = Array.from(xmlData.getElementsByTagName("item"));
-        console.log(sortSearch(gameList, name));
-        return sortSearch(gameList, name);
+        console.log(gameList);
+        sortSearch(gameList, name);
+        console.log(gameList);
+        gameList.forEach((game) => {
+            return SearchResultCard(game);
+        }
+        );
+
+        // return sortSearch(gameList, name);
         ;
     })
         .catch((err) => console.log("ERROR: ", err));
@@ -15,7 +24,7 @@ const searchGameByName = (name) => {
 
 const clearSearch = () => {
     document.addEventListener('DOMContentLoaded', () => {
-        document.getElementById('result-content').style.display = "none";
+        document.getElementById('result-content').classList.add('hidden');
         document.getElementById('result-content').innerHTML = "";
     });
 }
@@ -44,6 +53,7 @@ const searchGame = (e, game) => {
     if(e.key === "Enter"){
       searchGameByName(game);
     }
+    document.getElementById('result-content').classList.remove('hidden');
   }
 
-  export { searchGameByName, clearSearch, searchGame}
+  export { searchGameByName, clearSearch, searchGame }
